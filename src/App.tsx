7c07c4c623befdaa644e351b4c6b508eb9dedb52
Haritiana Adriano <103512767+haritianaadriano@users.noreply.github.com>
2024-10-16 11:18:48 +0300

    feat: movies filtered by emotion
    * feat: select emotion for list movies
    
    * feat: fetched movies resources

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,35 @@ import { Skeleton } from '@mui/material';
 import { client } from './provider/client';
 import { Movie } from './provider/types/Movie';
 import CustomCard from './components/CustomCard';
+import emotionOptions from './provider/types/EmotionOptions';
 
 function App() {
   const [isLoading, setIsLoading] = useState<Boolean>();
+  const [emotion, setEmotion] = useState<string>();
   const [movie, setMovie] = useState<Movie>();
+  const [movies, setMovies] = useState<Movie[]>();
 
-  const handleClick = async () => {
+  const handleChange = (e: React.FormEvent<HTMLSelectElement>) => {
+    setEmotion(e.currentTarget.value);
+    console.log(emotion);
+  };
+
+  const handleClick1 = async (emotion: string) => {
+    try {
+      const data = await client
+        .get<Movie[]>(`/movies?emotion=${emotion}`)
+        .then((response) => response.data);
+
+      setMovies(data);
+      console.log(movies);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleClick0 = async () => {
     setIsLoading(true);
     try {
       const data = await client
@@ -40,23 +63,54 @@ function App() {
     <>
       <header>
         <div>
-          <h1 className='text-2xl md:text-4xl'>Screenscape</h1>
-          <h3>Retrieve your movie</h3>
+          <h1 className='text-3xl md:text-5xl'>Screenscape</h1>
+          <h2 className='text-xl md:text-2xl'>Retrieve your movie</h2>
+          <p className='mb-2 mt-5'>Your best friend to get randomly movies</p>
         </div>
       </header>
       <div>
         {isLoading ? (
           <Skeleton variant='rounded' width={250} height={200} />
         ) : movie ? (
-          <CustomCard handleClick={handleClick} item={movie} />
+          <CustomCard handleClick={handleClick0} item={movie} />
         ) : (
           <button
-            onClick={handleClick}
+            onClick={handleClick0}
             className='rounded border-b-4 border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:border-blue-500 hover:bg-blue-400'
           >
             Suggest me
           </button>
         )}
+      </div>
+
+      <div className='flex'>
+        <div className='mt-4'>
+          <label className='mb-2 mt-10 block text-sm font-medium text-gray-900 dark:text-white'>
+            We will give you movies for your feeling
+          </label>
+          <form className='mx-auto max-w-sm'>
+            <select
+              id='countries'
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
+              onChange={handleChange}
+            >
+              {emotionOptions.map((emotionOption) => (
+                <option value={emotionOption.value}>
+                  {emotionOption.label}
+                </option>
+              ))}
+            </select>
+          </form>
+        </div>
+
+        <div className='ml-5 mt-20'>
+          <button
+            className='rounded border-b-4 border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:border-blue-500 hover:bg-blue-400'
+            onClick={() => handleClick1(emotion!)}
+          >
+            List me movies
+          </button>
+        </div>
       </div>
     </>
   );
---
+++ src/provider/types/EmotionOptions.ts
@@ -0,0 +1,24 @@
+const emotionOptions = [
+  {
+    label: 'Tell we how you feel',
+    value: 'HAPPY',
+  },
+  {
+    label: 'Happy',
+    value: 'HAPPY',
+  },
+  {
+    label: 'Sad',
+    value: 'SAD',
+  },
+  {
+    label: 'Angry',
+    value: 'ANGRY',
+  },
+  {
+    label: 'In love',
+    value: 'IN_LOVE',
+  },
+];
+
+export default emotionOptions;
